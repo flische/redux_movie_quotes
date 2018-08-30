fb2660b0157898721faf35c8874524cd63be5d36
flische <flische@gmail.com>
2018-08-29 17:59:45 -0700

    routing setup complete

---
+++ src/components/secret_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">List of Operatives</h1>
+        <ol>
+            <li>Jake</li>
+            <li>Sudip</li>
+            <li>Tien</li>
+            <li>Ashley</li>
+            <li>Archer</li>
+        </ol>       
+    </div>
+); 
