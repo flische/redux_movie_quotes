2c9152449b77fcb1f0c47ff8ca6fba71941bd169
flische <flische@gmail.com>
2018-08-31 12:12:55 -0700

    basic auth complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer} from 'redux-form';
-import { userReducer } from './user_reducer';
+import userReducer from './user_reducer';
 
 const rootReducer = combineReducers({
     form: formReducer,
--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -4,12 +4,12 @@ const DEFAULT_STATE = {
     auth: false
 };
 
-export default (state = DEFAULT_STATE, actions) => {
+export default (state = DEFAULT_STATE, action) => {
     switch(action.type){
         case types.SIGN_IN:
             return { auth: true };
         case types.SIGN_OUT:
-            return {auth: false };    
+            return { auth: false };    
         default:
             return state;
     }
